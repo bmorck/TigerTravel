1631d3d6826bd478723ea3897cf18c7d67f7357b
Donovan Coronado <donovancoronado@nat-oitwireless-inside-vapornet100-10-9-125-97.princeton.edu>
2019-04-04 16:12:43 -0400

    database stuff

--- tigertravel/models.py
+++ tigertravel/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
-# Create your models here.
+# class Group(models.Model):
+# 	destination =
+# 	date =
+# 	start_time =
+# 	end_time =
+# 	members = models.ManytoMany?
+
+# class Request(models.Model):
+# 	destination =
+# 	date =
+# 	start_time =
+# 	end_time =
+# 	person = models.ForeignKey(User)
+
+# class User(models.Model):
+# 	user =
+# 	netid =
+# 	email =
+# 	photo = ?
+
+class Post(models.Model):
+	title = models.CharField(max_length=100)
+	content = models.TextField()
+	date_posted = models.DateTimeField(default=timezone.now)
+	author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+	def __str__(self):
+		return self.title
--- tigertravel/views.py
+++ tigertravel/views.py
@@ -1,26 +1,13 @@
 from django.shortcuts import render
+from .models import Post
 
-posts = [
-	{
-		'author': 'me',
-		'title': 'one post',
-		'content': 'blah blah blah',
-		'date_posted': 'March 31, 2019'
-	},
-	{
-		'author': 'you',
-		'title': 'two post',
-		'content': 'blah blah blah',
-		'date_posted': 'April 1, 2019'
-	}
-]
 
 def home(request):
 	return render(request, 'tigertravel/mainpage.html')
 
 def about(request):
 	context = {
-		'posts': posts,
+		'posts': Post.objects.all(),
 		'title': 'About'
 	}
 	return render(request, 'tigertravel/about.html', context)
