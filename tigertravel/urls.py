feaba30108fadc21ebdec71e7b37261d4da73ada
Donovan Coronado <donovancoronado@nat-oitwireless-inside-vapornet100-10-9-125-97.princeton.edu>
2019-04-06 14:30:49 -0400

    implement grouping algorithm v1 and make active groups tab

--- tigertravel/urls.py
+++ tigertravel/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import RequestCreateView, RequestListView
+from .views import RequestCreateView, RequestListView, GroupListView
 from . import views
 
 urlpatterns = [
     path('', RequestCreateView.as_view(), name='tigertravel-home'),
     path('about/', RequestListView.as_view(), name='tigertravel-about'),
+    path('groups/', GroupListView.as_view(), name='tigertravel-groups'),
 ]
--- tigertravel/views.py
+++ tigertravel/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from .models import Request
+from .models import Request, Group
 from django.views.generic import CreateView, ListView
 from django.contrib import messages
 
@@ -12,10 +12,36 @@ class RequestCreateView(CreateView):
 		form.instance.person = self.request.user
 		return super().form_valid(form)
 
+	def get_success_url(self):
+		changed = False
+		for group in Group.objects.all():
+			if group.date == self.object.date and group.destination == self.object.destination:
+				if self.object.end_time > group.start_time and self.object.start_time < group.end_time:
+					#work on max group number
+					changed = True
+					if self.object.start_time > group.start_time:
+						group.start_time = self.object.start_time
+					if self.object.end_time < group.end_time:
+						group.end_time = self.object.end_time
+					#add member name
+					group.save()
+					break
+
+		if changed == False:
+			new_group = Group(destination=self.object.destination, 
+				date=self.object.date, start_time=self.object.start_time,
+				end_time=self.object.end_time)
+			new_group.save()
+		return super().get_success_url()
+
 class RequestListView(ListView):
 	model = Request
 	template_name = 'tigertravel/about.html'
 	context_object_name = 'posts'
+	ordering = ['date']
+
+class GroupListView(ListView):
+	model = Group
 	ordering = ['date']
 
 
