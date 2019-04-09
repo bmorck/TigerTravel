d6fda2b8cb8090806b31961a68fcaa4513ffc6a8
Bobby Morck <32784880+bmorck@users.noreply.github.com>
2019-04-08 21:59:46 -0400

    Update 3

--- django_web/settings.py
+++ django_web/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -124,3 +125,5 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
+django_herku.settings(locals())
