91f2ea97da35c524a17b7196a345f10cc3463c4d
Bobby Morck <32784880+bmorck@users.noreply.github.com>
2019-04-23 19:34:49 -0400

    works?

--- tigertravel/urls.py
+++ tigertravel/urls.py
@@ -5,7 +5,7 @@ from uniauth.decorators import login_required
 from users import views as user_views
 
 urlpatterns = [
-    path('', RequestCreateView.as_view(), name='tigertravel-home'),
+    path('', login_required(RequestCreateView.as_view()), name='tigertravel-home'),
     path('listings/', login_required(RequestListView.as_view()), name='tigertravel-listings'),
     path('groups/', login_required(GroupListView.as_view()), name='tigertravel-groups'),
     path('groups/<int:pk>', login_required(GroupDetailView.as_view()), name='group-detail'),
